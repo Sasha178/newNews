975b6bd4e1d71e31bc5ff57bed86eb03ed0ca24f
aleksandrWeb <shurik90_08@mail.ru>
2021-01-25 16:36:13 +0300

    Реализация логики в базе данных

---
+++ database/migrations/2021_01_24_100846_create_news_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('external_id');
+            $table->string('img')->nullable();
+            $table->string('original_link');
+            $table->text('text')->nullable();
+            $table->boolean('partners_news');
+            $table->timestamp('news_time');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+            $table->index('external_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
